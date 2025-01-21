0b41329607ba8d7dd560f5442940df2d7a53e9c8
AnewG <breakitwall@gmail.com>
2025-01-21 21:46:18 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 #![deny(clippy::all)]
 
 use napi_derive::napi;
+use v_htmlescape::escape;
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
+pub fn escape_html(input: String) -> String {
+  escape(&input).to_string()
 }
